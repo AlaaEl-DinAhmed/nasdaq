03a830c29c90aa821d7d216afb8cb7bf329420ee
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-05-08 14:39:05 +0200

    feat: caching stock api response

--- apps/nasdaq/src/app/overmind/stocks/effects.ts
+++ apps/nasdaq/src/app/overmind/stocks/effects.ts
@@ -1,7 +1,8 @@
 export const api = {
   async getStocksFromServer() {
     const response = await fetch(
-      'https://api.polygon.io/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=50&apiKey=VXgEOdVKqYol4TPCABt1YFJzhZMxPn38'
+      'https://api.polygon.io/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=50&apiKey=VXgEOdVKqYol4TPCABt1YFJzhZMxPn38',
+      { cache: 'force-cache' }
     );
     const data = await response.json();
     return data.results;
