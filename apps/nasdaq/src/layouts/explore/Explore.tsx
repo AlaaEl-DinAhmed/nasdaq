e2121fa1083c1bccb865e51072decccf9bf15c89
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-05-08 22:37:30 +0200

    feat: adding explore screen ui

--- apps/nasdaq/src/layouts/explore/Explore.tsx
+++ apps/nasdaq/src/layouts/explore/Explore.tsx
@@ -1,14 +1,33 @@
+import React from 'react';
 import styled from 'styled-components';
 import Search from '../../components/search/Search';
 import StockBasicInfo from '../../components/stock-basic-info/StockBasicInfo';
 
 const Explore = () => {
-  const Wrapper = styled.article``;
+  const Header = styled.header`
+    background-color: var(--color-main-background);
+  `;
+  const Main = styled.main`
+    background-color: var(--color-secondary-background);
+    height: 100vh;
+  `;
+  const Ul = styled.ul`
+    padding-inline-start: var(--padding-start);
+  `;
+  const Li = styled.li``;
   return (
-    <Wrapper>
-      <Search />
-      <StockBasicInfo />
-    </Wrapper>
+    <React.Fragment>
+      <Header>
+        <Search />
+      </Header>
+      <Main>
+        <Ul>
+          <Li>
+            <StockBasicInfo />
+          </Li>
+        </Ul>
+      </Main>
+    </React.Fragment>
   );
 };
 
