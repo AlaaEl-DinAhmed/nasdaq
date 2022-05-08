f0c789c56d06e917a129caca0316b544bfb719d5
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-05-09 00:23:01 +0200

    feat: adding search component ui

--- apps/nasdaq/src/components/search/Search.tsx
+++ apps/nasdaq/src/components/search/Search.tsx
@@ -1,5 +1,50 @@
+import styled from 'styled-components';
+import SearchIcon from '../../assets/icons/icons-search.svg';
 const Search = () => {
-  return <div>Search</div>;
+  const Wrapper = styled.div`
+    padding-block-start: 1rem;
+    padding-block-end: 1rem;
+    padding-inline-start: var(--padding-start);
+    position: relative;
+  `;
+  const Span = styled.span`
+    position: absolute;
+    left: 47px;
+    bottom: 20px;
+  `;
+  const Image = styled.img`
+    width: 80%;
+  `;
+  const Input = styled.input`
+    background-color: #2f9cfe;
+    color: var(--color-main-text);
+    padding-block: 0.5rem;
+    padding-inline-start: 2.5rem;
+    padding-inline-end: 1rem;
+    border-radius: 4px;
+    border: 1px solid transparent;
+    line-height: 1;
+    ::placeholder {
+      color: var(--color-main-text);
+    }
+    :focus {
+      outline: none;
+      border-color: #fff;
+    }
+  `;
+  return (
+    <Wrapper>
+      <Span>
+        <Image alt="search icon" src={SearchIcon} />
+      </Span>
+      <Input
+        type="text"
+        placeholder="Search"
+        enterKeyHint="search"
+        inputMode="search"
+      />
+    </Wrapper>
+  );
 };
 
 export default Search;
