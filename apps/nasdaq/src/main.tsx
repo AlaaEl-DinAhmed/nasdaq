75f5624df1e79534cf1b4f33bbfe2e40635cd5b5
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-05-12 01:40:21 +0200

    feat: lazyloading stock details component

--- apps/nasdaq/src/main.tsx
+++ apps/nasdaq/src/main.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'overmind-react';
-import React from 'react';
+import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
@@ -15,22 +15,22 @@ const StockDetails = React.lazy(
 );
 
 root.render(
-  // <StrictMode>
-  <Provider value={overmind}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/stock-details/:id"
-          element={
-            <React.Suspense fallback={<div>Loading...</div>}>
-              <StockDetails />
-            </React.Suspense>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-    <GlobalStyles />
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider value={overmind}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route
+            path="/stock-details/:id"
+            element={
+              <React.Suspense fallback={<div>Loading...</div>}>
+                <StockDetails />
+              </React.Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      <GlobalStyles />
+    </Provider>
+  </StrictMode>
 );
