bd7404649180ac1fce147bb9545906c434d9644e
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-05-12 01:36:34 +0200

    feat: lazyloading stock details component

--- apps/nasdaq/src/main.tsx
+++ apps/nasdaq/src/main.tsx
@@ -1,9 +1,8 @@
 import { Provider } from 'overmind-react';
-import { StrictMode } from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import StockDetails from './components/stock-details/StockDetails';
 import { overmind } from './overmind/index';
 import { GlobalStyles } from './utils';
 
@@ -11,16 +10,27 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const StockDetails = React.lazy(
+  () => import('./components/stock-details/StockDetails')
+);
+
 root.render(
-  <StrictMode>
-    <Provider value={overmind}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/stock-details/:id" element={<StockDetails />} />
-        </Routes>
-      </BrowserRouter>
-      <GlobalStyles />
-    </Provider>
-  </StrictMode>
+  // <StrictMode>
+  <Provider value={overmind}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/stock-details/:id"
+          element={
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <StockDetails />
+            </React.Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+    <GlobalStyles />
+  </Provider>
+  // </StrictMode>
 );
