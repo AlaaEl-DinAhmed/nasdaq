b6599cd2b67940dc0e3641c93040feffdcdc3a16
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-05-14 22:18:02 +0200

    fix: ui issues, stock prev close data order

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,9 @@
 import * as express from 'express';
-import stocksRouter from './app/routers/stocks';
 
 const app = express();
 
-app.use('/api', stocksRouter);
-
 const port = process.env.port || 3333;
+
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
--- apps/nasdaq/src/models/prev-close.model.ts
+++ apps/nasdaq/src/models/prev-close.model.ts
@@ -3,15 +3,15 @@ import { IPrevCloseDetails } from '../interfaces/prev-close.interface';
 export default class PrevCloseModel implements IPrevCloseDetails {
   constructor(
     public close: number,
-    public open: number,
-    public low: number,
     public high: number,
+    public low: number,
+    public open: number,
     public volume: number
   ) {
     this.close = close;
-    this.open = open;
-    this.low = low;
     this.high = high;
+    this.low = low;
+    this.open = open;
     this.volume = volume;
   }
 }
